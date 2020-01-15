c4d36105bb947790795844e17a4e72241d523641
2b0f7bbd <est@localhost.localdomain>
2020-01-15 18:38:53 +0500

    Updates

---
+++ precache-manifest.273bfc727a66d5cfb83d4df6a48008aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b873efc498052584bd1cc18a9abea05",
+    "url": "/react_git_pages/index.html"
+  },
+  {
+    "revision": "81488a3a020dbe3685f0",
+    "url": "/react_git_pages/static/css/main.a1bd0fe5.chunk.css"
+  },
+  {
+    "revision": "946f02536b8ac7d01716",
+    "url": "/react_git_pages/static/js/2.49e96494.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_git_pages/static/js/2.49e96494.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81488a3a020dbe3685f0",
+    "url": "/react_git_pages/static/js/main.6fd225b5.chunk.js"
+  },
+  {
+    "revision": "e4fa2493a17f93bb39fa",
+    "url": "/react_git_pages/static/js/runtime-main.e37419c8.js"
+  }
+]);
